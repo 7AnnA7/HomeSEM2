9d38eb113488295134bd343a5860a38c1d681fdc
AnnA <anna_elfimova@mail.ru>
2022-07-13 16:27:51 +0300

    task 10 demo vtoraya zifra chisla abc

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// demo vtoraya zifra chisla abc
+Console.WriteLine("Введите трёхзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N >= 100 && N <= 999)
+{
+Console.WriteLine($"Вторая цифра этого числа {(N/10)%10} ");
+}
+else
+{
+    Console.WriteLine("Введённое Вами число не совсем трёхзначное");
+}
