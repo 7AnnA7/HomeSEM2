9a984eb5b84ac08ecb4019260bcfb4fa14c60744
AnnA <anna_elfimova@mail.ru>
2022-07-13 18:10:21 +0300

    task 13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// thurd zifra chisla libo net tret`ei zifri
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N <= 100)
+{
+ Console.WriteLine("Cообщаю, что третьей цифры в Вашем числе нет");
+}
+
+while (N>=1000)
+{
+  N=N/10;
+}
+
+if (N >= 100 && N <= 999)
+{
+ Console.WriteLine($"Третья цифра этого числа {N%10}");
+}
