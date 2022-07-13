9f80d51d7b9ee3a49999f32513cf2d5528c35a13
AnnA <anna_elfimova@mail.ru>
2022-07-13 18:11:24 +0300

    task 15

---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int den = Convert.ToInt32(Console.ReadLine());
+
+if (den >=1 && den <=5 )
+{
+  Console.WriteLine("Это не выходной день");
+}
+
+if (den == 6 || den == 7)
+{
+  Console.WriteLine("ВЫХОДНОЙ");
+}
+
+if (den <1 || den >7)
+{
+  Console.WriteLine("Нет такого дня недели");
+}
